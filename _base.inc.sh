0eebec6ae2474f296716ad1088876540947848d4
Malte Bublitz <malte70+github@mcbx.de>
2015-11-07 21:36:58 +0100

    Added OS detection to _base.inc.sh

--- _base.inc.sh
+++ _base.inc.sh
@@ -68,6 +68,30 @@ version() {
 }
 
 # 
+# OS detection
+# 
+
+OS=`uname -s`
+OSVERSION=`uname -r`
+if [[ "$OS" == "Darwin" ]]; then
+	OS="Mac OS X"
+	OSVARIANT=$OS
+	OSXVersion=`python -c 'import platform; print platform.mac_ver()[0],'`
+	OSVERSION=$OSXVersion
+elif [[ `uname -o` == "Cygwin" ]]; then
+	OS="Windows NT"
+	OSVERSION=`python -c 'import OSDetect; _i = OSDetect.OSInfo(); print _i.GetInfo()["OSVersion"]'`
+else
+	OS=`uname -o`
+	if which lsb_release &>/dev/null; then
+		OSVARIANT=`lsb_release -s -i`
+	else
+		OSVARIANT=$OS
+	fi
+	OSVERSION=`uname -r`
+fi
+
+# 
 # Set $EDITOR if not already set, so the scripts can expect it usable
 # 
 if [[ -z $EDITOR ]]; then
