f93308759fc43e595dda849a0f185b27aadb50a2
Malte Bublitz <malte@rolltreppe3.de>
2022-12-01 18:09:34 +0100

    [_base.inc.sh] ✨📚 Added ANSI escape codes and coments
    - Added descriptive comments for all functions
    - Added comments to separate sections of the script
    - Added more ANSI escape codes
      - Reset-codes for all attributes
      - New attributes:
        1. _ANSI_ATTR_STRIKETHROUGH
        2. _ANSI_ATTR_INVERSE
        3. _ANSI_ATTR_BLINKING
        4. _ANSI_ATTR_HIDDEN
        5. _ANSI_ATTR_CURSOR_VISIBLE/_ANSI_ATTR_CURSOR_HIDDEN
        6. _ANSI_ATTR_SCREEN_SAVE/_ANSI_ATTR_SCREEN_RESTORE
        7. _ANSI_ATTR_ALTERNATE_BUFFER_ENABLED/_ANSI_ATTR_ALTERNATE_BUFFER_DISABLED
    - Restructured the code that ensures a usable $EDITOR

--- _base.inc.sh
+++ _base.inc.sh
@@ -5,11 +5,64 @@
 # Basic features for my shell scripts
 # 
 
+
+
+##########################################
+# ANSI Escape Codes
+# 
+
+# 
+# The ESC character (U+001B)
+# 
 _ANSI_ESCAPE=$(printf "\e")
+
+# 
+# Reset all formatting
+# 
 _ANSI_RESET="${_ANSI_ESCAPE}[0m"
+
+# 
+# (Classic) Attributes and their reset codes
+# 
 _ANSI_ATTR_BOLD="${_ANSI_ESCAPE}[1m"
+_ANSI_ATTR_BOLD_RESET="${_ANSI_ESCAPE}[21m"
 _ANSI_ATTR_ITALIC="${_ANSI_ESCAPE}[3m"
-_ANSI_ATTR_UNDERLINE="${_ANSI_ESCAPE}4m"
+_ANSI_ATTR_ITALIC_RESET="${_ANSI_ESCAPE}[23m"
+_ANSI_ATTR_UNDERLINE="${_ANSI_ESCAPE}[4m"
+_ANSI_ATTR_UNDERLINE_RESET="${_ANSI_ESCAPE}[24m"
+_ANSI_ATTR_STRIKETHROUGH="${_ANSI_ESCAPE}[9m"
+_ANSI_ATTR_STRIKETHROUGH_RESET="${_ANSI_ESCAPE}[29m"
+# Switch foreground<->Background colors
+_ANSI_ATTR_INVERSE="${_ANSI_ESCAPE}[7m"
+_ANSI_ATTR_INVERSE_RESET="${_ANSI_ESCAPE}[27m"
+# Annoying, but included for the sake of completeness
+_ANSI_ATTR_BLINKING="${_ANSI_ESCAPE}[5m"
+_ANSI_ATTR_BLINKING_RESET="${_ANSI_ESCAPE}[25m"
+# Like _ANSI_ATTR_BLINKING; I don't even know any practical use for this…
+_ANSI_ATTR_HIDDEN="${_ANSI_ESCAPE}[8m"
+_ANSI_ATTR_HIDDEN_RESET="${_ANSI_ESCAPE}[28m"
+
+# 
+# Advanced "attributes"/functionality
+# WARNING: No detection if the terminal supports these!
+# 
+# - ESC[?25h    make cursor visible
+# - ESC[?25l    make cursor invisible
+# - ESC[?47h    save screen
+# - ESC[?47l    restore screen
+# - ESC[?1049h  enables the alternative buffer
+# - ESC[?1049l  disables the alternative buffer
+# 
+_ANSI_ATTR_CURSOR_VISIBLE="${_ANSI_ESCAPE}[?25h"
+_ANSI_ATTR_CURSOR_HIDDEN="${_ANSI_ESCAPE}[?25l"
+_ANSI_ATTR_SCREEN_SAVE="${_ANSI_ESCAPE}[?47h"
+_ANSI_ATTR_SCREEN_RESTORE="${_ANSI_ESCAPE}[?47l"
+_ANSI_ATTR_ALTERNATE_BUFFER_ENABLED="${_ANSI_ESCAPE}[?1049h"
+_ANSI_ATTR_ALTERNATE_BUFFER_DISABLED="${_ANSI_ESCAPE}[?1049l"
+
+# 
+# Colors (original 16 colors)
+# 
 _ANSI_COLOR_BLACK="${_ANSI_ESCAPE}[0;30m"
 _ANSI_COLOR_RED="${_ANSI_ESCAPE}[0;31m"
 _ANSI_COLOR_GREEN="${_ANSI_ESCAPE}[0;32m"
@@ -27,32 +80,50 @@ _ANSI_COLOR_MAGENTA="${_ANSI_ESCAPE}[1;35m"
 _ANSI_COLOR_CYAN="${_ANSI_ESCAPE}[1;36m"
 _ANSI_COLOR_WHITE="${_ANSI_ESCAPE}[1;37m"
 
+
+
+##########################################
+# Functions
+# 
+
+# 
+# For internal use
+# 
+# Write to stdout and support escape-codes
 _print() {
 	#printf "$@"
 	echo -en "$@"
 }
+# Like _print, but add a newline
 _println() {
 	#printf "$@"
 	#echo
 	echo -e "$@"
 }
+# Write only to a real terminal
 _print_term() {
 	if [[ $TERM != "dump" ]]; then
 		_print "$@"
 	fi
 }
+# _print_term with a leading newline
 _println_term() {
 	if [[ $TERM != "dump" ]]; then
 		_println "$@"
 	fi
 }
 
+# 
+# Functions for use in scripts
+# 
+# A regular message
 message() {
 	_print_term $_ANSI_COLOR_GREEN
 	_print "[${SCRIPT_NAME}] "
 	_print_term $_ANSI_RESET
 	_println "$@"
 }
+# An error message (on stderr)
 message_error() {
 	(
 		_print_term $_ANSI_COLOR_RED
@@ -61,6 +132,7 @@ message_error() {
 		_println "$@"
 	) >&2
 }
+# A debug notice (on stderr)
 message_debug() {
 	(
 		_print_term $_ANSI_COLOR_YELLOW
@@ -69,12 +141,16 @@ message_debug() {
 		_println "$@"
 	) >&2
 }
-
+# Support "--version"/"-V" options by default
+# See also the if statement at the end of this file,
+# which checks if one of the options where passed.
 version() {
 	echo "$SCRIPT_NAME $SCRIPT_VERSION"
 }
 
-# 
+
+
+##########################################
 # OS detection
 # 
 
@@ -98,26 +174,32 @@ else
 	OSVERSION=`uname -r`
 fi
 
+
+
+##########################################
+# Ensure $EDITOR is set and usable
 # 
-# Set $EDITOR if not already set, so the scripts can expect it usable
-# 
+
 if [[ -z $EDITOR ]]; then
-	if ! which vim &>/dev/null; then
-		if ! which nano &>/dev/null; then
-			if ! which vi &>/dev/null; then
-				message_error "Neither vim, nor vi, nor nano found!"
-				exit 1
-			else
-				EDITOR="vi"
-			fi
-		else
-			EDITOR="nano"
-		fi
-	else
+	if [ -x /etc/alternatives/editor ]; then
+		EDITOR="editor"
+	elif which vim &>/dev/null; then
 		EDITOR="vim"
+	elif which nano &>/dev/null; then
+		EDITOR="nano"
+	elif which vi &>/dev/null; then
+		EDITOR="vi"
+	else
+		message_error "Neither vim, nor vi, nor nano found!"
+		exit 1
 	fi
 fi
 
+
+
+##########################################
+# Support for "--version"/"-V" options
+# 
 
 if [[ "$1" == "--version" || "$1" == "-V" ]]; then
 	version
